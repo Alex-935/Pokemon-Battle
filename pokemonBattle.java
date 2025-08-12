269e3c5fe0211c544a241f75fe7f73ef40b56f33
Alex <69348601+Alex-935@users.noreply.github.com>
2025-08-12 09:04:48 +0100

    Create Pokemon

--- pokemonBattle.java
+++ pokemonBattle.java
@@ -1,9 +1,16 @@
+
 public class pokemonBattle {
 
     public static void main(String[] args) {
 
+        Pokemon userPokemon = new Pokemon("Rayquaza", 50, "Dragon", 105, 150, 90, 150, 90, 95);
+        Pokemon compPokemon = new Pokemon("Garchomp", 50, "Dragon", 108, 130, 95, 80, 85, 102);
+        
 
         
     }
+
+
+
 
 }
